a28a31daac8b3c2347b8ace9aee6c629c9041b9e
Levi <ljheidrick@gmail.com>
2016-06-14 11:34:24 -0500

    added files, removed files, made various other changes

--- blog/dbconfig.py
+++
@@ -1,6 +0,0 @@
-db_user = "root"
-db_password = "akasis12"
-
-aws_prod_host = "mysql-blog-dbinstance.cbsvb1n2khfm.us-east-1.rds.amazonaws.com"
-aws_prod_user = "ljheidrick"
-aws_prod_password = "akasis12"
