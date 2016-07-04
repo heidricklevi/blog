de71525f9d9f980d964021f974a460cb775e05c1
Levi <ljheidrick@gmail.com>
2016-07-04 09:45:30 -0500

    Made small changes to app.js to initialize form data

--- blog/static/js/app.js
+++ blog/static/js/app.js
@@ -2,53 +2,55 @@
  * Created by LeviJamesH on 7/3/2016.
  */
 
-var main = function () {
-    "use strict";
-
-
-    var beforeName = document.getElementById("name").value;
-    console.log("Before" + beforeName);
-
-    document.getElementById("name").value = document.getElementById("name_td").innerHTML;
-
-    var name = document.getElementById("name");
-
-
-
-
-};
-
+//$(function (){
+//
+//    var table = document.getElementById("admin_table");
+//    console.log(table);
+//
+//
+//    for (var i = 0, row; row = table.rows[i]; i++) {
+//   //iterate through rows
+//   //rows would be accessed using the "row" variable assigned in the for loop
+//        console.log(row);
+//    for (var j = 0, col; col = row.cells[j]; j++) {
+//     //iterate through columns
+//     //columns would be accessed using the "col" variable assigned in the for loop
+//
+//        console.log(col);
+//   }
+//}
+//
+//
+//});
 
 $(function () {
     $("td").click(function () {
         var sib = this.nextElementSibling;
         var extraSibling = sib.getElementsByTagName('input');
+        console.log("Sib = " );
+        console.log(sib);
+        console.log("Extra Sib = ");
+        console.log(extraSibling);
 
-        //console.log(extraSibling);
+
         $(this).children().first().focus();
         var contents = $(this).html();
-        console.log(contents);
+        console.log("Original Contents = " + contents);
 
         $(this).blur(function () {
-            if (contents != $(this).html() && extraSibling.length == 0) {
-                window.alert("Contents have changed");
+            if (extraSibling.length == 0) {
                 sib.value = $(this).html();
-
-                console.log($(this).html());
+                console.log("Sib New Value: ");
                 console.log(sib);
             }
 
-            else {
-                extraSibling = sib.getElementsByTagName('input').item(0)
-
-                //window.alert("ExtraSibling active");
-                console.log($(this).html());
+            else if(extraSibling.length > 0) {
+                extraSibling = sib.getElementsByTagName('input').item(0);
                 extraSibling.value = $(this).html();
-                console.log(extraSibling.value);
+                console.log("Extra Sibling New Value = ");
+                console.log(extraSibling);
             }
         });
 
     });
 });
-
-$(document).ready(main);
